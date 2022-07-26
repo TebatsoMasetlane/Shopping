6dd831b512341cb86d931262ffd59821c39810f6
TEBATSO MASETLANE <99644910+TebatsoMasetlane@users.noreply.github.com>
2022-07-26 09:57:36 +0200

    Add files via upload

---
+++ src/components/Data.js
@@ -0,0 +1,56 @@
+export const Data = [
+  {
+    id: 1,
+    image: "shirts.jpg",
+    name: "Formal 5 Piece Mens Long Sleeve Dress Shirts",
+    price: 450,
+  },
+  {
+    id: 2,
+    image: "V-NECK COMPRESSION T-SHIRT.jpg",
+    name: "Fashion Red V neck PARIS SAINT GERMAIN",
+    price: 300,
+  },
+  {
+    id: 3,
+    image: "Designer T-shirts For Men.jpg",
+    name: "Fashion Designer T-shirts For Men",
+    price: 250,
+  },
+  {
+    id: 4,
+    image: "stylish shirt.jpg",
+    name: "Fashion Men Shirt Dress Suit Casual  Slim Fit",
+    price: 800,
+  },
+  {
+    id: 5,
+    image: "Unisex jersey.jpg",
+    name: "Unisex Hoodie with White Colour Only",
+    price: 400,
+  },
+  {
+    id: 6,
+    image: "cotton.jpg",
+    name: "Cotton Jersey for Women",
+    price: 250,
+  },
+  {
+    id: 6,
+    image: "All for women.jpg",
+    name: "All for Women Casual Clothing",
+    price: 450,
+  },
+  {
+    id: 7,
+    image: "men watch.jpg",
+    name: "Men Watch ",
+    price: 400,
+  },
+  {
+    id: 8,
+    image: "women watch.jpg",
+    name: "Women Watch With Pink Belts Only",
+    price: 250,
+  },
+];
